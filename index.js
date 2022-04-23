994351e27701b911668d43231ecccdd493aeb6d6
FredGuiou <frederic.guiou@gmail.com>
2022-04-23 19:02:21 +0200

    Etape 3.1 OK

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express');
 dotenv.config();
 
 const PORT = process.env.PORT || 1234;
+
+const session = require('express-session');
+
 const router = require('./app/router');
 
 const app = express();
@@ -11,6 +14,8 @@ app.set('view engine', 'ejs');
 app.set('views', 'app/views');
 
 app.use(express.static('public'));
+
+app.use(session({secret: process.env.SESSION_SECRET, resave: true, saveUninitialized: true}));
 
 app.use(router);
 
